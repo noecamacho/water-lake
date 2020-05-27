44f7dbed34ff6c7cde4d3560b8be7a60eeaa08f7
Noe Camacho <noecl1@hotmail.com>
2020-05-26 21:19:45 -0500

    basic functionality

---
+++ src/views/modulos/footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+	
+	<strong>>Copyright &copy; 2020 <a href="http://www.upsin.edu.mx" target="_blank">Universidad Polítecnia de Sinaloa</a>
+	</strong>
+
+	Todos los derechos reservados.
+
+</footer>
